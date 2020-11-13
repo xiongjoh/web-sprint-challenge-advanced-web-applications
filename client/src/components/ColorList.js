95a392ad923e12ec5a933a2d6e6137d9bdf7a762
unknown <xiongjoh@gmail.com>
2020-11-13 15:08:16 -0600

    move commented code

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -12,44 +12,8 @@ const initialColor = {
 
 const ColorList = () => {
   const { colorList, setColorList } = useContext(ColorContext)
-  // const [editing, setEditing] = useState(false);
-  // const [colorToEdit, setColorToEdit] = useState(initialColor);
 
   const {editing, setEditing, colorToEdit, setColorToEdit, editColor, saveEdit, deleteColor} = useColorEdit(initialColor)
-
-  // const editColor = color => {
-  //   setEditing(true);
-  //   setColorToEdit(color);
-  // };
-
-  // const saveEdit = e => {
-  //   console.log(colorToEdit.id)
-  //   e.preventDefault();
-  //   // Make a put request to save your updated color
-  //   // think about where will you get the id from...
-  //   // where is is saved right now?
-  //   axiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit)
-  //   .then(res => {
-  //     console.log(res.data)
-  //     setColorList(colorList.map(color => color.id !== colorToEdit.id ? color : res.data))
-  //   })
-  //   .catch(err => {
-  //     console.log(err)
-  //   })
-
-  // };
-
-  // const deleteColor = color => {
-  //   // make a delete request to delete this color
-  //   axiosWithAuth().delete(`/colors/${color.id}`)
-  //   .then(res => {
-  //     console.log(res)
-  //     setColorList(colorList.filter(col => col.id !== res.data))
-  //   })
-  //   .catch(err => {
-  //     console.log(err)
-  //   })
-  // };
 
   return (
     <div className="colors-wrap">
@@ -111,3 +75,41 @@ const ColorList = () => {
 };
 
 export default ColorList;
+
+
+  // const [editing, setEditing] = useState(false);
+  // const [colorToEdit, setColorToEdit] = useState(initialColor);
+
+  // const editColor = color => {
+  //   setEditing(true);
+  //   setColorToEdit(color);
+  // };
+
+  // const saveEdit = e => {
+  //   console.log(colorToEdit.id)
+  //   e.preventDefault();
+  //   // Make a put request to save your updated color
+  //   // think about where will you get the id from...
+  //   // where is is saved right now?
+  //   axiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit)
+  //   .then(res => {
+  //     console.log(res.data)
+  //     setColorList(colorList.map(color => color.id !== colorToEdit.id ? color : res.data))
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+
+  // };
+
+  // const deleteColor = color => {
+  //   // make a delete request to delete this color
+  //   axiosWithAuth().delete(`/colors/${color.id}`)
+  //   .then(res => {
+  //     console.log(res)
+  //     setColorList(colorList.filter(col => col.id !== res.data))
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   })
+  // };
